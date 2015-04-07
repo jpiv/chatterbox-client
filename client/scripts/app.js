1bdbf6539abb0dcb48f4d6610a14ed7e1e6960d6
jpiv <jjpizzo4@gmail.com>
2015-04-06 19:53:27 -0700

    Passed all specs except for events

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,91 @@
 // YOUR CODE HERE:
 
+var App = function() {
+  this.server = 'https://api.parse.com/1/classes/chatterbox';
 
-$('document').ready(function (){
+}
 
+var app = new App();
 
-var message = {
-  'username': 'frankbowers',
-  'text': 'This is April 6 16:07 test',
-  'roomname': '6th floor'
+App.prototype.init = function() {
+  // var $appScript = $('.appScript');
+  // $appScript.before('<script src="env/config.js"></script>');
+  // var that = this;
+  // $appScript.ready(function () {
+  //   that.fetch();
+  // });
+  this.fetch();
+}
+
+App.prototype._userClick = function () {
+  this.addFriend();
 };
+
+App.prototype.addFriend = function () {
+
+};
+
+App.prototype.clearMessages = function() {
+  var $ul = $('#chats');
+  $ul.empty();
+}
+
+App.prototype.showMessages = function (data) {
+  var length = data.results.length;
+  for(var i = 0; i < length; i++) {
+    var msgData = data.results[i];
+    this.addMessage(msgData);
+  }
+  $('time.timeago').timeago();
+};
+
+App.prototype.addMessage = function(message) {
+  var $ul = $('#chats');
+  var source   = $('#msgTemplate').html();
+  var userClickCallback = this._userClick.bind(this);
+  if (source) {
+    var template = Handlebars.compile(source);
+    var msgHTML = template(message);
+    var $msg = $($.parseHTML(msgHTML));
+    $msg.on('click', userClickCallback);
+    $ul.append($msg);
+  }
+};
+
+App.prototype.addRoom = function(roomname) {
+  var $roomSelect = $('#roomSelect');
+  $roomSelect.append('<option value="' + roomname + '">' + roomname + '</option>');
+  //<option value="value1">Value 1</option>
+}
+
+App.prototype.fetch = function() {
+  var testLimit = { limit: 100, order: '-updatedAt'};
+  var that = this;
   $.ajax({
     // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
+    url: this.server,
+    type: 'GET',
+    data: testLimit,
+    contentType: 'application/json',
+    success: function (data){
+      that.showMessages(data);
+    },
+    error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message: ' + JSON.stringify(data));
+    }
+  });
+};
+
+App.prototype.send = function(message) {
+  // var message = {
+  //   'username': 'frankbowers',
+  //   'text': 'This is April 6 16:07 test',
+  //   'roomname': '6th floor'
+  // };
+  $.ajax({
+    // always use this url
+    url: this.server,
     type: 'POST',
     data: JSON.stringify(message),
     contentType: 'application/json',
@@ -24,40 +98,15 @@ var message = {
       console.error('chatterbox: Failed to send message: ' + JSON.stringify(data));
     }
   });
+};
 
-  var testLimit = { limit: 10000, order: '-updatedAt'};
 
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'GET',
-    data: testLimit,
-    contentType: 'application/json',
-    success: function (data){
-      showMessages(data);
-    },
-    error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message: ' + data);
-    }
-  });
 
+
+
+$('document').ready(function (){
+ app.init();
 
 });
 
-var showMessages = function (data) {
-
-  var $ul = $('.messages');
-  var length = data.results.length;
-  for(var i = 0; i < length; i++) {
-    var msgData = data.results[i];
-
-    var source   = $('#msgTemplate').html();
-    var template = Handlebars.compile(source);
-    var msgHTML = template(msgData);
-    var $msg = $($.parseHTML(msgHTML));
-    $ul.append($msg);
-  }
-  $('time.timeago').timeago();
-};
 
